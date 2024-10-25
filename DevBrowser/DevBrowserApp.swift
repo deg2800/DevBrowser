59aeea868d3b2c7bb3892d2cf752eb439f0359e0
Derrick Goodfriend <deg2800@mac.com>
2024-10-24 20:00:12 -0400

    Initial Commit

---
+++ DevBrowser/DevBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevBrowserApp.swift
+//  DevBrowser
+//
+//  Created by Derrick Goodfriend on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct DevBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
